7713284b5fa7f9f157c8ef721e6942c2d6fb65b9
abrahaam <abrahaam@LinuxX64>
2018-07-14 12:23:04 +0200

    redesign

---
+++ src/ssa/cloudplatform/Binary/Main.java
@@ -0,0 +1,29 @@
+package ssa.cloudplatform.Binary;
+
+public class Main {
+
+	public static void printHex(int value) {
+		System.out.printf("%x\n", value);
+	}
+
+	public static void printBin(int value) {
+		System.out.printf(Integer.toBinaryString(value) + "\n");
+	}
+
+	public static void main(String[] args) {
+
+//		int color = 0xff00ff;
+//		int r = (color & 0xff0000) >> 16;
+//		int g = (color & 0x00ff00) >> 8;
+//		int b = (color & 0xff) >> 0;
+//		printHex(color);
+//		printHex(r);
+//		printHex(g);
+//		printHex(b);
+//		int shuffle = 0x000000;
+//		shuffle = (b << 16) | (r << 8) | g;
+//		printHex(shuffle);
+		
+	}
+
+}
