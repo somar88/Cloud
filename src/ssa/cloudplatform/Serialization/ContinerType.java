5846fe15a2787a52e120e37c606aca2427b956b7
somar <somar@Extreme-Laptop.fritz.box>
2018-08-07 21:08:59 +0200

    #10 End

--- src/ssa/cloudplatform/Serialization/ContinerType.java
+++ src/ssa/cloudplatform/Serialization/ContinerType.java
@@ -6,6 +6,7 @@ public class ContinerType {
 	public static final byte FIELD	 	= 1;
 	public static final byte ARRAY 		= 2;
 	public static final byte OBJECT 	= 3;
+	public static final byte DATABASE	= 4;
 	
 
 }
