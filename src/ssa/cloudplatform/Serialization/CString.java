1f0fe6540cbd335396da1e4dc60c0f8e9b8f53aa
somar <somar@Extreme-Laptop.fritz.box>
2018-08-10 19:30:03 +0200

    #12 - 13:27

--- src/ssa/cloudplatform/Serialization/CString.java
+++ src/ssa/cloudplatform/Serialization/CString.java
@@ -1,6 +1,57 @@
 package ssa.cloudplatform.Serialization;
 
+import static ssa.cloudplatform.Serialization.SerializationWriter.writeBytes;
+
 public class CString {
 
+	public static final byte CONTAINER_TYPE = ContinerType.STRING; // data storage type(field, array, object)
+	public short nameLenght;
+	public byte[] name;
+	public int size = 1 + 2 + 4 + 4;
+	public int count;
+	private char[] characters;
+
+	private CString() {
+	}
+
+	public void setName(String name) {
+		assert (name.length() < Short.MAX_VALUE);
+
+		if (this.name != null)
+			size -= this.name.length;
+
+		nameLenght = (short) name.length();
+		this.name = name.getBytes();
+		size += nameLenght;
+	}
+
+	private void updateSize() {
+		size += getDataSize();
+	}
+
+	public int getBytes(byte[] dest, int pointer) {
+		pointer = writeBytes(dest, pointer, CONTAINER_TYPE);
+		pointer = writeBytes(dest, pointer, nameLenght);
+		pointer = writeBytes(dest, pointer, name);
+		pointer = writeBytes(dest, pointer, count);
+		return pointer;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public int getDataSize() {
+		return characters.length * Type.getSize(Type.CHAR);
+	}
+
+	public static CString Create(String name, String data) {
+		CString string = new CString();
+		string.setName(name);
+		string.count = data.length();
+		string.characters = data.toCharArray();
+		string.updateSize();
+		return string;
+	}
 
 }
--- src/ssa/cloudplatform/Serialization/ContinerType.java
+++ src/ssa/cloudplatform/Serialization/ContinerType.java
@@ -5,8 +5,9 @@ public class ContinerType {
 	public static final byte UNKNOWN 	= 0;
 	public static final byte FIELD	 	= 1;
 	public static final byte ARRAY 		= 2;
-	public static final byte OBJECT 	= 3;
-	public static final byte DATABASE	= 4;
+	public static final byte STRING 		= 3;
+	public static final byte OBJECT 	= 4;
+	public static final byte DATABASE	= 5;
 	
 
 }
